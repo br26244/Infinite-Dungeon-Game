ab86e4bbb5012f564a318e224b866ecd69f46195
ahsu055 <ahsu055@ucr.edu>
2023-05-08 13:14:55 -0700

    Entity folder and Classes

---
+++ Entities/entity.h
@@ -0,0 +1,51 @@
+#ifndef __ENTITY_H__
+#define __ENTITY_H__
+#include "floor.h"
+#include "floor.cpp"
+
+class Entity{
+    private:
+
+        //Character Stats
+        int health;
+        int str;
+        int def;
+
+        //X,Y coords(?)
+        int xCoord
+        int yCoord
+
+    public:
+        //Entity default/initial constructor and 
+        Entity()
+        Entity(int health, int str, int def, int xCoord, int yCoord)
+
+        //Necessary Deconstructor
+        ~Entity()
+
+        //Entity movement checks 
+        void moveChar(Floor &floorData)
+        bool boundaryCheck(Floor &floorData, int xCoord, int yCoord)
+
+        //entity stat modification/use/setters/getters
+        int hpAccess()
+        int strAccess()
+        int defAccess()
+        int xAccess()
+        int yAccess()
+        
+
+        void modifyHealth(int hpMod)
+        void modifyStrength(int strMod)
+        void modifyDefense(int defMod)
+
+        //interactions with other objects/items
+        void attack(Entity oppTarget, int damage)
+
+    private:
+        
+
+        
+};
+
+#endif
