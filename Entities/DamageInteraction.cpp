2e4d31b7db36678248ad4a57263abd5c7a209862
ahsu055 <ahsu055@ucr.edu>
2023-06-09 15:16:43 -0700

    Made sure too much def does not heal the enemy or player

--- Entities/DamageInteraction.cpp
+++ Entities/DamageInteraction.cpp
@@ -7,24 +7,39 @@
 
 void DamageInteraction::entityDamage(Enemy& targetEnemy, Entity &targetChar)
 {
-    targetEnemy.setHealth(targetEnemy.getHealth()-(targetChar.strAccess()-targetEnemy.getDefense()));
-    cout << "Player struck for " << (targetChar.strAccess()-targetEnemy.getDefense()) << " damage!!\n"; 
-    if(targetEnemy.getHealth() <= 0)
+    if(targetEnemy.getDefense() <= targetChar.strAccess())
     {
-        targetEnemy.setDeathState(true);
-        cout << "Enemy has been slain!\n";
-        targetChar.modifyExp(5);
-        cout << "You have gained 5 exp from this encounter!\n";
+        cout << "Enemy blocked incoming damage!!\n";
     }
     else
     {
-        targetChar.modifyHealth(-(targetEnemy.getAttack()-targetChar.defAccess()));
-        cout << "Player was hit for " << (targetEnemy.getAttack()-targetChar.defAccess()) << " damage!!\n";
-        if(targetChar.hpAccess() <= 0)
+        targetEnemy.setHealth(targetEnemy.getHealth()-(targetChar.strAccess()-targetEnemy.getDefense()));
+        cout << "Player struck for " << (targetChar.strAccess()-targetEnemy.getDefense()) << " damage!!\n"; 
+        if(targetEnemy.getHealth() <= 0)
         {
-            targetChar.modifyDeathState(true);
-            cout << "Player has been slain!\n";
-            //targetEnemy.modifyExp(targetChar1.expAccess());
+            targetEnemy.setDeathState(true);
+            cout << "Enemy has been slain!\n";
+            targetChar.modifyExp(5);
+            cout << "You have gained 5 exp from this encounter!\n";
+        }
+        else
+        {
+            if(targetChar.defAccess() <= targetEnemy.getAttack())
+            {
+                cout << "Player blocked incoming damage!!\n";
+            }
+            else
+            {
+                targetChar.modifyHealth(-(targetEnemy.getAttack()-targetChar.defAccess()));
+                cout << "Player was hit for " << (targetEnemy.getAttack()-targetChar.defAccess()) << " damage!!\n";
+                if(targetChar.hpAccess() <= 0)
+                {
+                    targetChar.modifyDeathState(true);
+                    cout << "Player has been slain!\n";
+                //targetEnemy.modifyExp(targetChar1.expAccess());
+                }
+            }
         }
     }
+    
 }
