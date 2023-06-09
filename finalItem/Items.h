58ce3a068ba0c4c1576f8a2eee3ecfa5cdeda294
br26244 <brios86439@gmail.com>
2023-06-09 03:40:20 -0700

    adding items files

---
+++ finalItem/Items.h
@@ -0,0 +1,39 @@
+#include <string>
+#include "Entity.h"
+#ifndef __ITEMS_H__
+#define __ITEMS_H__
+using namespace std;
+
+class Item
+{
+
+public:
+	Item(int itemId, int xPoint, int yPoint);
+	string getName();
+	int getHealAmount();
+	int getStrUp();
+	int getDefUp();
+	int getEXP();
+	int getId();
+	int getX();
+	int getY();
+	void modPlayer(Entity& player);
+	bool used;
+
+private:
+	string name;
+	string description;
+
+	int id;
+	char sym;
+	int healAmount;
+	int healthLossAmount;
+	int strengthUp;
+	int defenceUp;
+	int exp;
+	
+	int x;
+	int y;
+
+};
+#endif
